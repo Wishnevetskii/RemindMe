e08870ca85468f6a089a34e25160cd57b6be8aee
Michael Wishnevetskii <michaelushka@gmail.com>
2020-01-28 23:40:54 +0400

    test differences

--- app/src/main/java/com/mcherry/remindme/MainActivity.java
+++ app/src/main/java/com/mcherry/remindme/MainActivity.java
@@ -3,12 +3,17 @@ package com.mcherry.remindme;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tvHello;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvHello = findViewById(R.id.tv_hello);
     }
 }
