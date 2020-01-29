dcdc980bf07cec1de810ce380af9baa2f3af79ed
Michael Wishnevetskii <michaelushka@gmail.com>
2020-01-29 10:03:24 +0400

    test differences

--- app/src/main/java/com/mcherry/remindme/OtherActivity.java
+++ app/src/main/java/com/mcherry/remindme/OtherActivity.java
@@ -3,12 +3,18 @@ package com.mcherry.remindme;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class OtherActivity extends AppCompatActivity {
+    private TextView textView;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_other);
+
+
     }
 }
